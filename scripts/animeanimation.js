9e7f7befc1bebffed2ebe219c37d1475ced202e9
Robert Hare <35596943+robertharedev@users.noreply.github.com>
2021-03-27 20:43:50 +0000

    Edited svg class names
    Changed subheading container names from subheading1/2 to subheading-container-1/2.
    Gave all paths that I want animated with line drawing a "lineDraw" class name so I could have svgs that wouldnt get drawn.

--- scripts/animeanimation.js
+++ scripts/animeanimation.js
@@ -1,10 +1,6 @@
-const svgPath = document.querySelectorAll("path");
-const svgFirstWordPath = document.querySelectorAll(".firstword");
-const svgSecondWordPath = document.querySelectorAll(".secondword");
-const subheading1Container = document.querySelector("#subheading1");
-const subheading2Container = document.querySelector("#subheading2");
 
 // line drawing animation
+const svgPath = document.querySelectorAll(".lineDraw");
 anime({
     targets: svgPath,
     strokeDashoffset: [anime.setDashoffset, 0],
@@ -16,6 +12,7 @@ anime({
 });
 
 // svg firstword fadein from left
+const svgFirstWordPath = document.querySelectorAll(".firstword");
 anime({
     targets: svgFirstWordPath,
     translateX: [-150, 0], // from -300 to 0
@@ -25,6 +22,7 @@ anime({
 });
 
 // svg secondword fadein from right
+const svgSecondWordPath = document.querySelectorAll(".secondword");
 anime({
     targets: svgSecondWordPath,
     translateX: [150, 0], // from 200 to 0
@@ -33,7 +31,8 @@ anime({
     duration: 2000
 });
 
-// subheadings fadein from above and below
+// subheading1 fadein from above
+const subheading1Container = document.querySelector("#subheading-container-1");
 anime({
     targets: subheading1Container,
     translateY: [-75, 0], // from 75 to 0
@@ -43,6 +42,8 @@ anime({
     delay: 1000, 
 });
 
+// subheading2 fadein from below
+const subheading2Container = document.querySelector("#subheading-container-2");
 anime({
     targets: subheading2Container,
     translateY: [75, 0], // from 75 to 0
@@ -50,4 +51,15 @@ anime({
     easing: "easeInOutExpo",
     duration: 2000,
     delay: 1500, 
+});
+
+// scroll symbol fadein from above
+const scrollSymbol = document.querySelector("#scroll-symbol-container");
+anime({
+    targets: scrollSymbol,
+    translateY: [-75, 0], // from 75 to 0
+    opacity: [0, 1],
+    easing: "easeInOutExpo",
+    duration: 2000,
+    delay: 2000, 
 });
