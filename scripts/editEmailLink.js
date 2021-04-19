d0693e67a6e50f4c681c9389a3b934ffca76d240
Robert Hare <35596943+robertharedev@users.noreply.github.com>
2021-04-19 18:30:18 +0100

    Small fixes
    - Aligned headings to centre so they don't wrap (go to next line) on mobile
    - Edited comments

--- scripts/editEmailLink.js
+++ scripts/editEmailLink.js
@@ -1,6 +1,6 @@
-// Edits the contact form's link so that my email is a bit more hidden from the user as it is more
-// difficult to find using Chrome Developer Tools than it would be if it were in the index.html file
-// (if you're on GitHub reading this... Well done.)
+// Edits the contact form's link so that my email is a bit more hidden from the user as it requires more clicking
+// to find it using Chrome Developer Tools than it would be if it were in the index.html file
+// (I know it doesn't really do much but it makes me feel better and if you're on GitHub reading this... See.)
 function sendEmail() {
     window.open("https://www.formsubmit.co/robert.hare36@gmail.com");
 }
