14de56077f31bb44d035043f34abf1d46f15d2cb
Robert Hare <35596943+robertharedev@users.noreply.github.com>
2021-04-19 18:43:19 +0100

    Implemented contact form functionality
    Now sets the action as the link to send the information in the form and then completes the action (opens the link to send the email)

--- scripts/editEmailLink.js
+++ scripts/editEmailLink.js
@@ -2,5 +2,7 @@
 // to find it using Chrome Developer Tools than it would be if it were in the index.html file
 // (I know it doesn't really do much but it makes me feel better and if you're on GitHub reading this... See.)
 function sendEmail() {
-    window.open("https://www.formsubmit.co/robert.hare36@gmail.com");
+    const form = document.getElementById("contact-form")
+    form.action = "https://formsubmit.co/robert.hare36@gmail.com";
+    form.submit();
 }
