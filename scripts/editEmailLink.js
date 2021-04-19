47c18e1e46c939aef1302b21364aaa610d3f9dd7
Robert Hare <35596943+robertharedev@users.noreply.github.com>
2021-04-19 19:31:38 +0100

    Update editEmailLink.js

--- scripts/editEmailLink.js
+++ scripts/editEmailLink.js
@@ -2,7 +2,8 @@
 // to find it using Chrome Developer Tools than it would be if it were in the index.html file
 // (I know it doesn't really do much but it makes me feel better and if you're on GitHub reading this... See.)
 function sendEmail() {
-    const form = document.getElementById("contact-form")
-    form.action = "https://formsubmit.co/robert.hare36@gmail.com";
-    form.submit();
+    const form = document.getElementById("contact-form")            // gets the form
+    form.action = "https://formsubmit.co/robert.hare36@gmail.com";  // changes the form's action link
+    form.submit();                                                  // submitting the form causes the form's action link to be opened
+    form.action = "javascript:sendEmail()";                         // sets the link back
 }
