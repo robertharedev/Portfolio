0bb136026c3c6dd72113d8c2d6bacdad30cbd391
Robert Hare <35596943+robertharedev@users.noreply.github.com>
2021-04-19 18:18:09 +0100

    Small security edit
    Hid email from user by putting it within JS file instead of index.html

---
+++ scripts/editEmailLink.js
@@ -0,0 +1,6 @@
+// Edits the contact form's link so that my email is a bit more hidden from the user as it is more
+// difficult to find using Chrome Developer Tools than it would be if it were in the index.html file
+// (if you're on GitHub reading this... Well done.)
+function sendEmail() {
+    window.open("https://www.formsubmit.co/robert.hare36@gmail.com");
+}
