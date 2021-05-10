e68a1d3bac109d68694fce2665e96085c7d7fe4d
Robert Hare <35596943+robertharedev@users.noreply.github.com>
2021-05-10 17:52:09 +0100

    Minor changes
    - Reordered/refactored animation functions
    - Made button border colours lighter

--- scripts/frontPageAnimation.js
+++ scripts/frontPageAnimation.js
@@ -1,66 +1,40 @@
-// navbar elements drop down onto page
-anime({
-    targets: ".nav-element",
-    translateY: [-200, 0],
-    opacity: [0, 1],
-    delay: function(el, i, l) { return (i * 200) + 1800; },
-    direction: "forwards",
-    easing: "easeInOutExpo",
-    duration: 2000,
-});
-
-// line drawing animation
-anime({
-    targets: ".lineDraw",
-    strokeDashoffset: [anime.setDashoffset, 0],
-    delay: function(el, i) { return i * 100 }, 
-    direction: "forwards",
-    easing: "easeInOutSine",
-    duration: 3000,
-});
-
-// svg firstword fadein from left
-anime({
-    targets: ".firstword",
-    translateX: [-150, 0], // from -300 to 0
-    opacity: [0, 1],
-    easing: "easeInOutSine",
-    duration: 2000,
-    complete: () => { // On completion of this animation, call this function
-        fillFirstWord();
-    }
-});
-
-function fillFirstWord() {
-    // fill first word orange
-    anime({
-        targets: ".firstword",
-        fill: '#ffba24',
-        easing: "easeInOutSine",
-        direction: "forwards",
-        delay: function(el, i, l) { return (i * 100); },
-        duration: 1000
-    });
-}
-
-// svg secondword fadein from right
-anime({
-    targets: ".secondword",
-    translateX: [150, 0], // from 200 to 0
-    opacity: [0, 1],
-    easing: "easeInOutSine",
-    duration: 2000
-});
-
 // subheading1 fadein from above
 anime({
     targets: "#subheading-container-1",
     translateY: [-75, 0], // from 75 to 0
     opacity: [0, 1],
     easing: "easeInOutExpo",
-    duration: 2000,
-    delay: 1000, 
+    duration: 2000
 });
+
+drawNameLogo();
+
+function drawNameLogo() {
+    // line drawing animation
+    anime({
+        targets: ".lineDraw",
+        strokeDashoffset: [anime.setDashoffset, 0],
+        direction: "forwards",
+        easing: "easeInOutSine",
+        opacity: [0, 1],
+        delay: 1000,
+        duration: 2000,
+        complete: () => { // On completion of this animation, call this function
+            fillFirstWord();
+        }
+    });
+    
+    function fillFirstWord() {
+        // fill first word orange
+        anime({
+            targets: ".firstword",
+            fill: '#ffba24',
+            easing: "easeInOutSine",
+            direction: "forwards",
+            duration: 500
+        });
+    }
+}
 
 // subheading2 fadein from below
 anime({
@@ -69,7 +43,7 @@ anime({
     opacity: [0, 1],
     easing: "easeInOutExpo",
     duration: 2000,
-    delay: 1500, 
+    delay: 2000
 });
 
 // scroll symbol fadein from above
